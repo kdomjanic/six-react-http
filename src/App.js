c918a15701f4f332854df179c254c7e4e16d3105
Karlo Domjanić <karlo.domjanic@sigit.hr>
2024-05-02 15:39:57 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,34 +4,51 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json()
-    }).then(data => {
-      const transformedMovies = data.results.map(movie => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        }
-      });
-      setMovies(transformedMovies)
-    })
-  }
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    async function fetchMoviesHandler() {
+        setIsLoading(true);
+        const response = await fetch('https://swapi.dev/api/films/');
+        const data = await response.json()
+        const transformedMovies = data.results.map(movie => {
+            return {
+                id: movie.episode_id,
+                title: movie.title,
+                openingText: movie.opening_crawl,
+                releaseDate: movie.release_date,
+            }
+        });
+        setMovies(transformedMovies);
+        setIsLoading(false);
+    }
+    // function fetchMoviesHandler(){
+    //     fetch('https://swapi.dev/api/films/').then(response => {
+    //         return response.json()
+    //     }).then(data => {
+    //         const transformedMovies = data.results.map(movie => {
+    //             return {
+    //                 id: movie.episode_id,
+    //                 title: movie.title,
+    //                 openingText: movie.opening_crawl,
+    //                 releaseDate: movie.release_date,
+    //             }
+    //         });
+    //         setMovies(transformedMovies)
+    //     })
+    // }
 
-
-  return (
-      <React.Fragment>
-        <section>
-          <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-        </section>
-        <section>
-          <MoviesList movies={movies} />
-        </section>
-      </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+                {isLoading && <p>Loading...</p>}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
